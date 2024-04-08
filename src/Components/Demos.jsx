8d696bc16c0f5747cff56a973cb69dbff958b0d4
Mohamed Magdy <mohammedmagdy6522@gmail.com>
2024-04-08 13:25:18 +0200

    Change projects component style

--- src/Components/Demos.jsx
+++ src/Components/Demos.jsx
@@ -1,54 +1,47 @@
-import './Style/Style.css'
 import React from 'react';
-import DemoImages from '../Data/Demos';
+import SwiperCore, { EffectCoverflow, Pagination, Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination, Navigation } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import arrow from '../Images/Icons/arrow.png'
-import forward from'../Images/Icons/forward.png'
+import 'swiper/swiper-bundle.min.css';
+import '../Style/Style.css';
+import DemoImages from '../Data/Demos';
+
+SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
 
 const Demos = () => {
   return (
-    <div className="container">
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        {DemoImages.map((item,index)=>(
-          <SwiperSlide >
-            <img src={item.project} className={`${item.color}`} alt="slide_image" />
-          </SwiperSlide>
-        ))}
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-          <img src={arrow} alt="" />
+    <div className="page">
+      <div className="container ">
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+          }}
+          pagination={{ clickable: true }}
+          navigation={{ clickable: true }}
+          className="swiper_container"
+        >
+          {DemoImages.map((item, index) => (
+            
+            <SwiperSlide key={index}>
+              <a href={item.link} target='_blank' rel='noreferrer'>
+                <img src={item.project} className={`bg-[#92C7CF] swiper-image`} alt="slide_image" />
+              </a>
+              <p className='text-lg font-mono'>{item.desc}</p>
+            </SwiperSlide>
+          ))}
+          <div className="slider-controler">
+          
+            <div className="swiper-pagination"></div>
           </div>
-          <div className="swiper-button-next slider-arrow">
-          <img src={forward} alt="" />
-          </div>
-          <div className="swiper-pagination"></div>
-        </div>
-      </Swiper>
+        </Swiper>
+      </div>
     </div>
   );
 };
--- src/Data/Demos.js
+++ src/Data/Demos.js
@@ -1,12 +1,12 @@
 import relu from '../Images/Demos/relu.jpg';
 import H_Scrub from "../Images/Demos/sa3y_copy.png";
 import Gosto from "../Images/Demos/Gosto.jpg";
-// import Auction from "../Images/Demos/Auction.png";
+import Auction from "../Images/Demos/Auction.png";
 import Loukma from "../Images/Demos/Loukma.jpg";
 import Cyper from "../Images/Demos/Cyper.jpg";
 // import Portfolio from "../Images/Demos/Portfolio.png";
 import cartemplate from "../Images/Demos/car-template.jpg";
-import niketemplete from "../Images/Demos/nike-templete.jpg";
+import niketemplete from "../Images/Demos/nike-templete.png";
 
 const DemoImages = [
     {
@@ -15,37 +15,35 @@ const DemoImages = [
       link:'https://scrub-shopping.vercel.app/',
       desc:'E-commerce Website',
       text:'Click on me',
-      color: 'bg-green-500'
+      
     },
     {
       project:relu,
       link:'https://relu-client.vercel.app/',
-      desc:'Cashier system',
+      desc:'E-commerce Website',
       text:'Click on me',
-      color: 'bg-green-500'
+      
     },
     {
       project:Gosto,
       desc:'E-commerce Website',
       text:'Link not available',
-      color: 'bg-green-500'
+      
     },
-    // {
-    //   project:Auction,
-    //   desc:'E-commerce Website',
-    //   text:'Link not available'
-    // },
+    {
+      project:Auction,
+      desc:'E-commerce Website',
+      text:'Link not available'
+    },
     {
       project:Cyper,
       desc:'Management system',
       text:'Link not available',
-      color: 'bg-blue-500'
     },
     {
       project:Loukma,
       desc:'Cashier system',
       text:'Link not available',
-      color: 'bg-blue-500'
     },
     // {
     //   project:Portfolio,
@@ -58,14 +56,13 @@ const DemoImages = [
       link:'https://car-template-rho.vercel.app/',
       desc:'E-commerce Website',
       text:'Click on me',
-      color: 'bg-green-500'
+      
     },
     {
       project:niketemplete,
       link:'https://nike-templete.vercel.app/',
       desc:'E-commerce Website',
       text:'Click on me',
-      color: 'bg-green-500'
     },
     
    ];
