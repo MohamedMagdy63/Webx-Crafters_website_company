f46c5c7b6ae432b2ec9471d7affd3c8cda037b29
ahmedgamal115 <ahmedgamal1153gmail.com>
2024-04-19 02:49:23 +0200

    update media

--- src/Components/MainSlide.jsx
+++ src/Components/MainSlide.jsx
@@ -158,10 +158,12 @@ const MainSlide = () => {
 
   useEffect(()=>{
     window.addEventListener("scroll", () => {
-      if(window.pageYOffset >= (mainContainer.current.offsetHeight / 2)){
-        setActiveCursur(true)
-      }else{
-        setActiveCursur(false)
+      if(mainContainer.current){
+        if(window.pageYOffset >= (mainContainer.current.offsetHeight / 2)){
+          setActiveCursur(true)
+        }else{
+          setActiveCursur(false)
+        }
       }
     });
   },[activeCursur])
@@ -208,52 +210,57 @@ const MainSlide = () => {
     <div ref={mainContainer} className="relative w-full lg:h-screen sm:h-screen lg:flex lg:justify-center lg:flex-row lg:items-center select-none cursor-none
     lg:before:content-normal lg:before:absolute lg:before:top-0 lg:before:right-0 lg:before:w-[60%] lg:before:h-full 
     lg:before:bg-[rgb(64,213,224)] lg:before:rounded-bl-full lg:before:z-10
+    
     sm:before:content-normal sm:before:absolute sm:before:top-0 sm:before:right-0 sm:before:w-[80%] sm:before:h-[70%] 
     sm:before:bg-[rgb(64,213,224)] sm:before:rounded-bl-full
-    sm:flex sm:flex-col-reverse sm:justify-center sm:items-center">
+    sm:flex sm:flex-col-reverse sm:justify-center sm:items-center
+
+    max-sm:before:content-normal max-sm:before:absolute max-sm:before:top-0 max-sm:before:right-0 max-sm:before:w-[70%] max-sm:before:h-[50%] 
+    max-sm:before:bg-[rgb(64,213,224)] max-sm:before:rounded-bl-full
+    max-sm:flex max-sm:flex-col-reverse max-sm:justify-end max-sm:items-end">
       <div ref={courser} className={`fixed top-0 left-0 translate-x-[-50%] translate-y-[-50%] w-[40px] h-[40px] rounded-full border-2 
       border-red-400 bg-[#ffff] ${styleState ? "mix-blend-difference" : "mix-blend-normala"} ${activeCursur ? 'hidden' : 'visible'} pointer-events-none z-30`}></div>
 
       <div ref={leftSection} className='lg:w-[60%] lg:h-screen lg:flex lg:justify-center lg:items-center lg:z-10
       sm:w-[90%] sm:h-screen sm:flex sm:justify-start sm:items-start sm:z-10 sm:pt-10
-      max-sm:w-[90%] max-sm:h-screen max-sm:flex max-sm:justify-center max-sm:items-center max-sm:z-10 max-sm:pt-10'>
+      max-sm:w-full max-sm:h-[60vh] max-sm:flex max-sm:justify-center max-sm:items-center max-sm:z-10 max-sm:pt-10'>
 
-        <div ref={textContainer} className='w-fit h-fit flex flex-col lg:justify-center lg:items-start lg:pl-[100px] sm:pl-0 sm:justify-start sm:items-start'>
+        <div ref={textContainer} className='w-fit h-fit flex flex-col lg:justify-center lg:items-start lg:pl-[100px] sm:pl-0 sm:justify-start sm:items-start max-sm:pl-0 max-sm:justify-start max-sm:items-start'>
           <div ref={designDiv} className='w-full text-start h-1 overflow-hidden'>
-            <p ref={designEle} className='lg:text-7xl sm:text-5xl font-mainFont'>Design, <span ref={test} className='font-bold text-mainColor'>Develop</span></p>
+            <p ref={designEle} className='lg:text-7xl sm:text-5xl max-sm:text-5xl font-mainFont'>Design, <span ref={test} className='font-bold text-mainColor'>Develop</span></p>
           </div>
           <div className='flex justify-center items-center gap-4 ml-14 '>
             <p ref={textAnd} className='font-bold font-mono text-black lg:text-8xl sm:text-6xl max-sm:text-6xl scale-0'>&</p>
             <p ref={Webpages} className={`font-bold font-mainFont lg:text-7xl sm:text-5xl max-sm:text-5xl relative `} 
             style={{WebkitTextFillColor:"transparent", WebkitTextStroke:"2px black",WebkitBackgroundClip:"text",}}>Create Webpages</p>
           </div>
-          <p ref={textSplit} className='lg:text-7xl sm:text-5xl sm:mt-6 max-sm:mt-6 max-sm:text-5xl font-mainFont'>Software <span className='font-bold text-mainColor'>Systems</span></p>
+          <p ref={textSplit} className='lg:text-7xl sm:text-5xl sm:mt-6 max-sm:mt-4 max-sm:text-4xl font-mainFont'>Software <span className='font-bold text-mainColor'>Systems</span></p>
         </div>
       </div>
-      <div className='lg:w-[60%] lg:h-screen sm:w-full sm:h-[50%] max-sm:w-[50%] max-sm:h-[50%] z-20'>
-        <div className='w-full h-full flex lg:justify-start lg:items-center sm:justify-end sm:items-end'>
+      <div className='lg:w-[60%] lg:h-screen sm:w-full sm:h-[50%] max-sm:w-[100%] max-sm:h-[100%] z-20'>
+        <div className='w-full h-full flex lg:justify-start lg:items-center sm:justify-end sm:items-end max-sm:justify-end max-sm:items-end'>
           <img 
           src="landingPage.png" 
           alt=""
-          className='object-cover lg:w-full lg:h-[90%] sm:w-[80%]' />
+          className='object-cover lg:w-full lg:h-[90%] sm:w-[80%] max-sm:w-full' />
         </div>
       </div>
 
       <div ref={container} className='absolute overflow-hidden bottom-0 w-full h-[100px] p-0  flex justify-evenly items-center'>
         <div ref={subdiv} className='flex justify-center items-center gap-4 w-[500px]'>
-          <img src={Layout} className='w-16 h-16 p-1' alt="" />
-          <p className="text-sm lg:text-2xl text-center">
+          <img src={Layout} className='lg:w-16 lg:h-16 sm:w-16 sm:h-16 max-sm:w-14 max-sm:h-14  p-1' alt="" />
+          <p className="text-sm lg:text-2xl max-sm:lg:text-xl text-center">
             Unique Layout design
           </p>
         </div>
         <div ref={subdiv} className='flex justify-center items-center gap-4 w-[500px]'>
-          <img src={Responsive} className='w-16 h-16 p-1' alt="" />
+          <img src={Responsive} className='lg:w-16 lg:h-16 sm:w-16 sm:h-16 max-sm:w-14 max-sm:h-14 p-1' alt="" />
           <p className="text-sm lg:text-2xl text-center">
             Beautiful all Device Responsive
           </p>
         </div>
         <div ref={subdiv} className='flex justify-center items-center gap-4 w-[500px]'>
-          <img src={TechnicalSupport} className='w-16 h-16 p-1' alt="" />
+          <img src={TechnicalSupport} className='lg:w-16 lg:h-16 sm:w-16 sm:h-16 max-sm:w-14 max-sm:h-14 p-1' alt="" />
           <p className="text-sm lg:text-2xl text-center">
             24/7 Hours Dedicated Support
           </p>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,40 +17,40 @@ const Navbar = () => {
     })
   })
   return (
-    <div className={`fixed top-0 left-0 w-full p-5 transition-all duration-300 ${activeBg ? "bg-slate-50" : "bg-none"} z-50`}>
+    <div className={`fixed top-0 left-0 w-full lg:p-5 sm:p-3 max-sm:p-3 transition-all duration-300 ${activeBg ? "bg-slate-200" : "bg-none"} z-50`}>
       <div className="w-full flex items-center relative z-10">
-          <Link className="w-[15%]" to='/'>
-            <span className=" text-4xl font-bold bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent">
+          <Link className="lg:w-[15%] sm:w-[15%] max-sm:w-[30%]" to='/'>
+            <span className="lg:text-4xl sm:text-2xl max-sm:text-lg font-bold bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent">
               W
             </span>
-            <span className="text-2xl font-mainFont">ebX-Crafters</span>
+            <span className="lg:text-2xl sm:text-xl max-sm:text-lg font-mainFont">ebX-Crafters</span>
           </Link>
 
         <div className="w-[85%] flex justify-between items-center">
             {/* Navigation Links */}
             <ul className='flex space-x-4 text-gray-500'>
-              <Link to='/about' className="text-2xl font-mainFont font-bold px-3 hover:text-gray-800">About</Link>
-              <Link to='/services' className="text-2xl font-mainFont font-bold px-3 hover:text-gray-800">Services</Link>
-              <Link to='/projects' className="text-2xl font-mainFont font-bold px-3 hover:text-gray-800">Projects</Link>
+              <Link to='/about' className="lg:text-2xl sm:text-2xl max-sm:text-lg font-mainFont font-bold px-3 hover:text-gray-800">About</Link>
+              <Link to='/services' className="lg:text-2xl sm:text-2xl max-sm:text-lg font-mainFont font-bold px-3 hover:text-gray-800">Services</Link>
+              <Link to='/projects' className="lg:text-2xl sm:text-2xl max-sm:text-lg font-mainFont font-bold px-3 hover:text-gray-800">Projects</Link>
             </ul>
 
             {/* Social Media Icons */}
             <ul className='flex flex-row space-x-4'>
-              <div className="relative w-12 h-12 rounded-full flex justify-center items-center p-2 border-2 border-black grayscale
+              <div className="relative lg:w-12 lg:h-12 sm:w-9 sm:h-9 max-sm:w-7 max-sm:h-7 rounded-full flex justify-center items-center lg:p-2 sm:p-2 max-sm:p-1 border-2 border-black grayscale
               hover:grayscale-0 
               before:content before:absolute before:w-0 before:h-0 before:bg-white before:rounded-full before:hover:w-full before:hover:h-full before:transition-all before:duration-500 before:z-[-1]">
                 <a href="https://www.instagram.com/webx_crafters" target="_blank" rel="noopener noreferrer">
                   <img src={instagram} alt="Instagram" />
                 </a>
               </div>
-              <div className="relative w-12 h-12 rounded-full flex justify-center items-center p-2 border-2 border-black grayscale
+              <div className="relative lg:w-12 lg:h-12 sm:w-9 sm:h-9 max-sm:w-7 max-sm:h-7 rounded-full flex justify-center items-center lg:p-2 sm:p-2 max-sm:p-1 border-2 border-black grayscale
               hover:grayscale-0 
               before:content before:absolute before:w-0 before:h-0 before:bg-white before:rounded-full before:hover:w-full before:hover:h-full before:transition-all before:duration-500 before:z-[-1]">
                 <a href="https://www.tiktok.com/@webx_crafters" target="_blank" rel="noopener noreferrer">
                   <img src={tiktok} alt="tiktok" />
                 </a>
               </div>
-              <div className="relative w-12 h-12 rounded-full flex justify-center items-center p-2 border-2 border-black grayscale
+              <div className="relative lg:w-12 lg:h-12 sm:w-9 sm:h-9 max-sm:w-7 max-sm:h-7 rounded-full flex justify-center items-center lg:p-2 sm:p-2 max-sm:p-1 border-2 border-black grayscale
               hover:grayscale-0 
               before:content before:absolute before:w-0 before:h-0 before:bg-white before:rounded-full before:hover:w-full before:hover:h-full before:transition-all before:duration-500 before:z-[-1]">
                 <a href="mailto:webxcrafters@gmail.com" target="_blank" rel="noopener noreferrer">
